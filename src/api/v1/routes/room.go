a9a33c6616ea600e031d439ce399c47a26867e71
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-28 02:14:28 -0600

    feat: CReate Routes by Rooms Operations

---
+++ src/api/v1/routes/room.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"fmt"
+
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/views"
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/config"
+)
+
+func Rooms() {
+	//Create Room
+	config.Router.POST(fmt.Sprintf("/api/%s/rooms", config.API_VERSION), views.Rooms_POST)
+}
