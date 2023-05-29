5a58e500cfb12016656fa46bc36fef8356c76abf
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-29 00:28:19 -0600

    feat: CReate Schemas by Computer

---
+++ src/api/v1/schemas/computer.go
@@ -0,0 +1,11 @@
+package schemas
+
+type ComputerCreateSchema struct {
+	Name   string `json:"name"`
+	IdRoom string `json:"id_room"`
+	Type   string `json:"type"`
+}
+
+type ComputerFindSchema struct {
+	IdComputer string `json:"id_computer"`
+}
