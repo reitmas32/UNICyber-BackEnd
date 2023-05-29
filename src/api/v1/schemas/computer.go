9f5c2c4e36a8f31bb4c7df04e7ba9071239779a7
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-29 01:53:05 -0600

    feat: Create ComputerUpdateSchema

--- src/api/v1/schemas/computer.go
+++ src/api/v1/schemas/computer.go
@@ -10,6 +10,18 @@ type ComputerFindSchema struct {
 	IdComputer string `json:"id_computer"`
 }
 
-type ComputerRenameSchema struct {
-	Name string `json:"name"`
+type ComputerUpdateSchema struct {
+	// Metadata
+	//IdComputer string `gorm:"primaryKey"`
+	//IdRoom     string
+
+	// UI
+	Pos_x float32 `json:"pos_x"`
+	Pos_y float32 `json:"pos_y"`
+
+	// Data
+	Name    string `json:"name"`
+	State   string `json:"state"`
+	Message string `json:"message"`
+	Type    string `json:"type"`
 }
