67cdd80c52abd189fb2afc5bc01b68761365d88f
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-30 15:20:54 -0600

    feat: Create Student model

--- src/models/student.go
+++ src/models/student.go
@@ -4,7 +4,16 @@ import "github.com/jinzhu/gorm"
 
 type Student struct {
 	gorm.Model
-	idUser   string
-	Password string
-	UserName string
+	//Metadata
+	IdStudent string `gorm:"primaryKey"`
+
+	//Info Personal
+	Name     string
+	LastName string
+	Email    string
+
+	//Info Academic
+	UniversityProgram string
+	AccountNumber     string
+	Semester          uint8
 }
