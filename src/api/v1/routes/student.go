eba55fc6a0c9071ac7f7ccebf2b36b1015720af9
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-30 15:19:43 -0600

    feat: Add Routes by Student

--- src/api/v1/routes/student.go
+++ src/api/v1/routes/student.go
@@ -1,26 +1,22 @@
 package routes
 
 import (
-	"net/http"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/views"
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/config"
 )
 
-func CreateStudent(c *gin.Context) {
-	c.Writer.WriteHeader(http.StatusAccepted)
-	c.Writer.Write([]byte("Error al obtener el contenido del archivo"))
-}
+func StudentRoutes() {
+	//Create Computer
+	config.Router.POST(fmt.Sprintf("/api/%s/student", config.API_VERSION), views.Student_POST)
 
-func UpdateStudent(c *gin.Context) {
-	c.Writer.WriteHeader(http.StatusAccepted)
-	c.Writer.Write([]byte("Error al obtener el contenido del archivo"))
-}
-func DeleteStudent(c *gin.Context) {
-	c.Writer.WriteHeader(http.StatusAccepted)
-	c.Writer.Write([]byte("Error al obtener el contenido del archivo"))
-}
+	//Get Computer
+	//config.Router.GET(fmt.Sprintf("/api/%s/computer/:id-computer", config.API_VERSION), views.Computer_GET)
 
-func GetStudent(c *gin.Context) {
-	c.Writer.WriteHeader(http.StatusAccepted)
-	c.Writer.Write([]byte("Error al obtener el contenido del archivo"))
+	//Update Computer
+	//config.Router.PUT(fmt.Sprintf("/api/%s/computer/:id-computer", config.API_VERSION), views.Computer_PUT)
+
+	//Delete Computer
+	//config.Router.DELETE(fmt.Sprintf("/api/%s/computer/:id-computer", config.API_VERSION), views.Computer_DELETE)
 }
