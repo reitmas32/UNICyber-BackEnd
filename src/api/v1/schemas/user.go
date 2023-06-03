202a7845fe7cfd26c74fdffdb0f3e2a1cccf93e5
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-03 08:05:57 -0600

    feta: Add binding to models

--- src/api/v1/schemas/user.go
+++ src/api/v1/schemas/user.go
@@ -1,17 +1,17 @@
 package schemas
 
 type UserSignInSchema struct {
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
+	UserName string `json:"user_name" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type UserSignUpSchema struct {
-	Name        string `json:"name"`
-	LastName    string `json:"last_name"`
-	Email       string `json:"email"`
-	UserName    string `json:"user_name"`
-	PhoneNumber string `json:"phone_number"`
-	DateOfBirth string `json:"date_of_birth"`
-	Password    string `json:"password"`
-	Role        string `json:"role"`
+	Name        string `json:"name" binding:"required"`
+	LastName    string `json:"last_name" binding:"required"`
+	Email       string `json:"email" binding:"required,email"`
+	UserName    string `json:"user_name" binding:"required"`
+	PhoneNumber string `json:"phone_number" binding:"required"`
+	DateOfBirth string `json:"date_of_birth" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	Role        string `json:"role" binding:"required"`
 }
