5e570e169b27133dd2c38580a5cf51614d8cbba3
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 03:23:10 -0600

    feat: create new Endpoints by Room

--- src/api/v1/routes/room.go
+++ src/api/v1/routes/room.go
@@ -10,4 +10,13 @@ import (
 func Rooms() {
 	//Create Room
 	config.Router.POST(fmt.Sprintf("/api/%s/rooms", config.API_VERSION), views.Room_POST)
+
+	//Get Room
+	config.Router.GET(fmt.Sprintf("/api/%s/room/:id-room", config.API_VERSION), views.Room_GET)
+
+	//Update Room
+	config.Router.PUT(fmt.Sprintf("/api/%s/room/:id-room", config.API_VERSION), views.Room_PUT)
+
+	//Delete Room
+	config.Router.DELETE(fmt.Sprintf("/api/%s/room/:id-room", config.API_VERSION), views.Room_DELETE)
 }
