e6e741381c4a61b38ea232f97d0c1df612f1a940
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-03 23:55:47 -0600

    feat: Add LinkAccount Endpoints

--- src/api/v1/routes/accounts.go
+++ src/api/v1/routes/accounts.go
@@ -14,3 +14,7 @@ func SignIn() {
 func SignUp() {
 	config.Router.PUT(fmt.Sprintf("/api/%s/signup", config.API_VERSION), views.SignUp_POST)
 }
+
+func LinkAccountWithComputerLab() {
+	config.Router.POST(fmt.Sprintf("/api/%s/link-account", config.API_VERSION), views.LinkAccount_POST)
+}
