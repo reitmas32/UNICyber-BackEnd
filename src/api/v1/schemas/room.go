58821787e4c13c6f2f302e8c1332227e5a797b26
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 02:12:45 -0600

    feat: Create new Schema by Update Room

--- src/api/v1/schemas/room.go
+++ src/api/v1/schemas/room.go
@@ -4,3 +4,11 @@ type RoomCreateSchema struct {
 	Name          string `json:"name"`
 	IdComputerLab string `json:"id_computer_lab"`
 }
+
+type RoomUpdateSchema struct {
+	// Metadata
+
+	// Data
+	Name  string `json:"name"`
+	Index string `json:"index"`
+}
