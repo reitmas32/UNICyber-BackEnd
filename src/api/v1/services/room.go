cc53dfd890cb8e25bf3d617fc7e6ee8745c24a60
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 02:34:16 -0600

    feat: CReate New Services

--- src/api/v1/services/room.go
+++ src/api/v1/services/room.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/schemas"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/config"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/models"
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/tools"
 )
 
 func CreateRoom(room models.Room) (bool, string) {
@@ -18,4 +20,39 @@ func CreateRoom(room models.Room) (bool, string) {
 	}
 
 	return true, "Create Room Successful"
+}
+
+func FindRoom(IdRoom string) (bool, string, models.Room) {
+
+	var room models.Room
+	if err := config.DB.First(&room, "id_room = ?", IdRoom).Error; err != nil {
+		return false, "No Find Room", room
+	}
+
+	return true, "Find Room", room
+}
+
+func DeleteRoom(IdRoom string) (bool, string, models.Room) {
+
+	var room models.Room
+	if err := config.DB.Delete(&room, "id_room = ?", IdRoom).Error; err != nil {
+		return false, "No Find Room", room
+	}
+
+	return true, "Delete Room", room
+}
+
+func UpdateRoom(IdRoom string, new_compuer schemas.RoomUpdateSchema) (bool, string, models.Room) {
+
+	var room models.Room
+	if err := config.DB.First(&room, "id_room = ?", IdRoom).Error; err != nil {
+		return false, "No Find Computer", room
+	} else {
+		room.Name = tools.CopyField(new_compuer.Name, room.Name, "")
+		room.Index = tools.CopyField(new_compuer.Index, room.Index, -1)
+
+		config.DB.Save(&room)
+	}
+
+	return true, "Change Name Successful", room
 }
