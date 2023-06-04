8f37554e51a7cf718f4872c454a4acd2e24cbda9
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 03:24:14 -0600

    feat: Create new Schema by Update a Student

--- src/api/v1/schemas/student.go
+++ src/api/v1/schemas/student.go
@@ -11,3 +11,15 @@ type StudentCreateSchema struct {
 	AccountNumber     string `json:"account_number" binding:"required,len=9,numeric"`
 	Semester          uint8  `json:"semester" binding:"required"`
 }
+
+type StudentUpdateSchema struct {
+	//Info Personal
+	Name     string `json:"name"`
+	LastName string `json:"last_name"`
+	Email    string `json:"email" binding:"email"`
+
+	//Info Academic
+	UniversityProgram string `json:"university_program"`
+	AccountNumber     string `json:"account_number" binding:",len=9,numeric"`
+	Semester          uint8  `json:"semester"`
+}
