578c18fb3bc76bb2aca3a8cf71a766905d16a8ae
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 02:07:26 -0600

    feat: Update Rooom_POST view

--- src/api/v1/views/rooms.go
+++ src/api/v1/views/rooms.go
@@ -2,118 +2,65 @@ package views
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/schemas"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/services"
-	"github.com/UNIHacks/UNIAccounts-BackEnd/src/config"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/models"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
-type Response struct {
-	Data    Data   `json:"Data"`
-	Message string `json:"Message"`
-	Success bool   `json:"Success"`
-}
+// @Summary add a new item of the rooms
+// @ID create-room
+// @Tags Rooms
+// @Produce json
+// @Param data body schemas.RoomCreateSchema true "Schema by Create New Room"
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Router /api/v1/computer [post]
+func Room_POST(c *gin.Context) {
 
-type Data struct {
-	Exp          int64  `json:"exp"`
-	ServiceName  string `json:"service_name"`
-	UserMotherID string `json:"user_mother_id"`
-	UserName     string `json:"user_name"`
-}
-
-func Rooms_POST(c *gin.Context) {
-
-	responseCreateRoom := map[string]interface{}{
-		"Message": "No Create Room",
-		"Data":    "{}",
-		"Success": false, // o false
+	responseCreateRoom := models.Response{
+		Message: "No Create Room",
+		Success: false,
+		Data:    "{}",
 	}
 
 	// Decodificar el objeto JSON recibido
-	var roomSchema schemas.RoomSchema
+	var roomSchema schemas.RoomCreateSchema
 	err := json.NewDecoder(c.Request.Body).Decode(&roomSchema)
 	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte("Error al obtener el contenido del archivo"))
-		return
-	}
-
-	// Crear un cliente HTTP personalizado con los headers deseados
-	client := &http.Client{}
-	//Todo Remplazar la url de desarrollo o producción
-	apiURL := fmt.Sprintf("http://127.0.0.1:%d/api/%s/validate_token", config.UNIACCOUNTS_API_PORT, config.UNIACCOUNTS_API_VERSION)
-	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte("Error al serializar los datos JSON"))
-		return
-	}
-
-	req, err := http.NewRequest(http.MethodGet, apiURL, nil)
-	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte("Error al crear la petición HTTP"))
-		return
-	}
-
-	// Añadir headers personalizados a la petición
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Api-Key", config.UNIACCOUNTS_API_KEY)
-	req.Header.Set("Service", config.UNIACCOUNTS_API_SERVICE_NAME)
-	req.Header.Set("Authorization", fmt.Sprintf("JWT %s", roomSchema.JWT))
-
-	// Realizar la petición HTTP con el cliente personalizado
-	resp, err := client.Do(req)
-
-	// Leer el contenido de la respuesta
-	if err != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte("Error al hacer la petición a la API externa"))
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err_json := ioutil.ReadAll(resp.Body)
-	if err_json != nil {
-		c.Writer.WriteHeader(http.StatusInternalServerError)
-		c.Writer.Write([]byte("Error al leer el cuerpo de la respuesta"))
-		return
-	}
-
-	var responseData map[string]interface{}
-	err = json.Unmarshal(body, &responseData)
-	if err != nil {
-		fmt.Println("Error al decodificar el cuerpo JSON:", err)
-		return
-	}
-
-	success := responseData["Success"].(bool)
-	if success {
-		var response Response
-		if err := json.Unmarshal(body, &response); err != nil {
-			fmt.Println("Error al analizar el JSON:", err)
-			return
+		responseCreateRoom := models.Response{
+			Message: "Error to Get Content JSON",
+			Success: false,
+			Data:    "{}",
 		}
+		c.Header("Content-Type", "application/json")
+		c.JSON(200, responseCreateRoom)
+		return
+	}
 
-		room := models.Room{
-			IdUser: response.Data.UserMotherID,
-			IdRoom: uuid.New().String(),
-			Name:   roomSchema.Name,
+	room := models.Room{
+		IdComputerLab: roomSchema.IdComputerLab,
+		IdRoom:        uuid.New().String(),
+		Name:          roomSchema.Name,
+	}
+
+	result, message := services.CreateRoom(room)
+
+	if result {
+
+		responseCreateRoom = models.Response{
+			Message: message,
+			Success: result,
+			Data:    room,
 		}
-
-		result, message := services.CreateRoom(room)
-
-		responseCreateRoom = map[string]interface{}{
-			"Message": message,
-			"Success": result,
-			"Data":    room,
+	} else {
+		responseCreateRoom = models.Response{
+			Message: message,
+			Success: responseCreateRoom.Success,
+			Data:    room,
 		}
-
 	}
 
 	c.Header("Content-Type", "application/json")
