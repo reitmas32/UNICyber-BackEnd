fc39662adec7d0a7ea36af8be861d0c806db3e0d
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 00:50:40 -0600

    docs: Add Doumentation and create Response Model

--- src/api/v1/views/computer_lab.go
+++ src/api/v1/views/computer_lab.go
@@ -10,29 +10,60 @@ import (
 	"github.com/google/uuid"
 )
 
-// @Summary add a new item to the computers
-// @ID create-computer
+// @Summary add a new item to the computer-lab
+// @ID create-computer-lab
+// @Tags Computer Lab
 // @Produce json
-// @Param data body schemas.ComputerCreateSchema true "todo data"
-// @Success 200
-// @Failure 400
-// @Router /api/v1/computer [post]
+// @Param data body schemas.ComputerLabCreateSchema true "todo data"
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Router /api/v1/computer-lab [post]
+// @Example responseSuccess
+// {
+//   "Message": "Create ComputerLab Successful",
+//   "Success": true,
+//   "Data": {
+//	   "Message": "Create ComputerLab Successful",
+//	   "Success": true,
+//	   "Data": {
+//	     "ID": 0,
+//	     "CreatedAt": "0001-01-01T00:00:00Z",
+//	     "UpdatedAt": "0001-01-01T00:00:00Z",
+//	     "DeletedAt": null,
+//	     "IdComputerLab": "3ed96d42-63a2-44d2-82a8-baf6f12fe80a",
+//	     "Name": "Sala 2",
+//	     "Description": ""
+//	   }
+//	 }
+// }
+// @Example responseError1
+// {
+//   "Message": "Error to Get Content JSON",
+//   "Success": false,
+//   "Data": "{}"
+// }
+// @Example responseError2
+// {
+//   "Message": "No Create Computer Lab",
+//   "Success": false,
+//   "Data": "{}"
+// }
 func ComputerLab_POST(c *gin.Context) {
 
-	responseCreateComputer := map[string]interface{}{
-		"Message": "No Create Computer Lab",
-		"Data":    "{}",
-		"Success": false,
+	responseCreateComputer := models.Response{
+		Message: "No Create Computer Lab",
+		Success: false,
+		Data:    "{}",
 	}
 
 	// Decodificar el objeto JSON recibido
 	var computerLabCreateSchema schemas.ComputerLabCreateSchema
 	err := json.NewDecoder(c.Request.Body).Decode(&computerLabCreateSchema)
 	if err != nil {
-		responseCreateComputer := map[string]interface{}{
-			"Message": "Error to Get Content JSON",
-			"Data":    "{}",
-			"Success": false,
+		responseCreateComputer := models.Response{
+			Message: "Error to Get Content JSON",
+			Success: false,
+			Data:    "{}",
 		}
 		c.Header("Content-Type", "application/json")
 		c.JSON(200, responseCreateComputer)
@@ -49,16 +80,17 @@ func ComputerLab_POST(c *gin.Context) {
 
 	if result {
 
-		responseCreateComputer = map[string]interface{}{
-			"Message": message,
-			"Success": result,
-			"Data":    computerLab,
+		responseCreateComputer = models.Response{
+			Message: message,
+			Success: result,
+			Data:    computerLab,
 		}
 	} else {
-		responseCreateComputer = map[string]interface{}{
-			"Message": message,
-			"Success": responseCreateComputer["Success"],
-			"Data":    computerLab,
+
+		responseCreateComputer = models.Response{
+			Message: message,
+			Success: responseCreateComputer.Success,
+			Data:    computerLab,
 		}
 	}
 
@@ -66,24 +98,40 @@ func ComputerLab_POST(c *gin.Context) {
 	c.JSON(200, responseCreateComputer)
 }
 
+// @Summary get a item to the computer-lab
+// @ID get-computer-lab
+// @Tags Computer Lab
+// @Produce json
+// @Param id-computer-lab path string true "ID del item"
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Router /api/v1/computer-lab [get]
 func ComputerLab_GET(c *gin.Context) {
 
-	result, message, computer := services.FindComputerLab(c.Param("id-computer-lab"))
+	result, message, computerLab := services.FindComputerLab(c.Param("id-computer-lab"))
 
-	responseGetComputerLab := map[string]interface{}{
-		"Message": message,
-		"Success": result,
-		"Data":    computer,
+	responseGetComputerLab := models.Response{
+		Message: message,
+		Success: result,
+		Data:    computerLab,
 	}
 
 	if !result {
-		responseGetComputerLab["Data"] = "{}"
+		responseGetComputerLab.Data = "{}"
 	}
 
 	c.Header("Content-Type", "application/json")
 	c.JSON(200, responseGetComputerLab)
 }
 
+// @Summary delete a item to the computer-lab
+// @ID delete-computer-lab
+// @Tags Computer Lab
+// @Produce json
+// @Param id-computer-lab path string true "ID del item"
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Router /api/v1/computer-lab [delete]
 func ComputerLab_DELETE(c *gin.Context) {
 
 	result, message, computerLab := services.FindComputerLab(c.Param("id-computer-lab"))
@@ -92,52 +140,60 @@ func ComputerLab_DELETE(c *gin.Context) {
 		result, message, _ = services.DeleteComputerLab(c.Param("id-computer-lab"))
 	}
 
-	responseDeleteComputerLab := map[string]interface{}{
-		"Message": message,
-		"Success": result,
-		"Data":    computerLab,
+	responseDeleteComputerLab := models.Response{
+		Message: message,
+		Success: result,
+		Data:    computerLab,
 	}
 
 	if !result {
-		responseDeleteComputerLab["Data"] = "{}"
+		responseDeleteComputerLab.Data = "{}"
 	}
 
 	c.Header("Content-Type", "application/json")
 	c.JSON(200, responseDeleteComputerLab)
 }
 
+// @Summary update a item to the computer-lab
+// @ID update-computer-lab
+// @Tags Computer Lab
+// @Produce json
+// @Param id-computer-lab path string true "ID del item"
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Router /api/v1/computer-lab [put]
 func ComputerLab_PUT(c *gin.Context) {
 
 	// Decodificar el objeto JSON recibido
 	var computerLabUpdateSchema schemas.ComputerLabUpdateSchema
 	err := json.NewDecoder(c.Request.Body).Decode(&computerLabUpdateSchema)
 	if err != nil {
-		responseUpdateComputerLab := map[string]interface{}{
-			"Message": "Error to Get Content JSON",
-			"Data":    "{}",
-			"Success": false,
+		responseUpdateComputerLab := models.Response{
+			Message: "Error to Get Content JSON",
+			Success: false,
+			Data:    "{}",
 		}
 		c.Header("Content-Type", "application/json")
 		c.JSON(200, responseUpdateComputerLab)
 		return
 	}
 
-	result, message, computerLab := services.FindComputerLab(c.Param("id-computer"))
+	result, message, computerLab := services.FindComputerLab(c.Param("id-computer-lab"))
 
 	if result {
 		result, message, computerLab = services.UpdateComputerLab(c.Param("id-computer-lab"), computerLabUpdateSchema)
 	}
 
-	responseRenameComputerLab := map[string]interface{}{
-		"Message": message,
-		"Success": result,
-		"Data":    computerLab,
+	responseUpdateComputerLab := models.Response{
+		Message: message,
+		Success: result,
+		Data:    computerLab,
 	}
 
 	if !result {
-		responseRenameComputerLab["Data"] = "{}"
+		responseUpdateComputerLab.Data = "{}"
 	}
 
 	c.Header("Content-Type", "application/json")
-	c.JSON(200, responseRenameComputerLab)
+	c.JSON(200, responseUpdateComputerLab)
 }
