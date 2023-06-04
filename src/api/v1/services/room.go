8801612108354b572497542ab2fb6fedbc635826
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 02:05:23 -0600

    feat: verify that it exists the computerLab

--- src/api/v1/services/room.go
+++ src/api/v1/services/room.go
@@ -7,6 +7,11 @@ import (
 
 func CreateRoom(room models.Room) (bool, string) {
 
+	var computerLab models.ComputerLab
+	if err := config.DB.First(&computerLab, "id_computer_lab = ?", room.IdComputerLab).Error; err != nil {
+		return false, "No Exist the ComputerLab"
+	}
+
 	result := config.DB.Create(&room)
 	if result.Error != nil {
 		return false, result.Error.Error()
