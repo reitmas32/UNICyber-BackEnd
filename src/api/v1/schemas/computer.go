53ff331e1aec79405ce4ffcc7148c0c216e29b02
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 04:33:45 -0600

    feat: Add examples to Schema

--- src/api/v1/schemas/computer.go
+++ src/api/v1/schemas/computer.go
@@ -1,9 +1,12 @@
 package schemas
 
+// @Summary Ejemplo de estructura A
+// @Description Descripción de la estructura A
+// @Tags Computer
 type ComputerCreateSchema struct {
-	Name   string `json:"name"`
-	IdRoom string `json:"id_room"`
-	Type   string `json:"type"`
+	Name   string `json:"name" example:"Equipo 2"`
+	IdRoom string `json:"id_room" example:"62dcb6fc-0f68-4a50-9d8c-8fe352b0f7f3"`
+	Type   string `json:"type" example:"Impresora"`
 }
 
 type ComputerFindSchema struct {
@@ -16,12 +19,12 @@ type ComputerUpdateSchema struct {
 	//IdRoom     string
 
 	// UI
-	Pos_x float32 `json:"pos_x"`
-	Pos_y float32 `json:"pos_y"`
+	Pos_x float32 `json:"pos_x" example:"156.29"`
+	Pos_y float32 `json:"pos_y" example:"157.30"`
 
 	// Data
-	Name    string `json:"name"`
-	State   string `json:"state"`
-	Message string `json:"message"`
-	Type    string `json:"type"`
+	Name    string `json:"name" example:"Equipo 3"`
+	State   string `json:"state" example:"Disponible"`
+	Message string `json:"message" example:"Solo falta instalar DevC++"`
+	Type    string `json:"type" example:"Prestamo"`
 }
