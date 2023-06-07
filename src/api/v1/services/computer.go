22951b5031f01f9b1770e2d4159d688b0e499072
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-07 10:12:42 -0600

    feat: change types of returs

--- src/api/v1/services/computer.go
+++ src/api/v1/services/computer.go
@@ -4,21 +4,17 @@ import (
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/api/v1/schemas"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/config"
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/models"
+	"github.com/UNIHacks/UNIAccounts-BackEnd/src/tools"
 )
 
-func CreateComputer(computer models.Computer) (bool, string) {
-
-	var room models.Room
-	if err := config.DB.First(&room, "id_room = ?", computer.IdRoom).Error; err != nil {
-		return false, "No existe la Aula"
-	}
+func CreateComputer(computer models.Computer) (bool, string, models.Computer) {
 
 	result := config.DB.Create(&computer)
 	if result.Error != nil {
-		return false, result.Error.Error()
+		return false, result.Error.Error(), computer
 	}
 
-	return true, "Create Computer Successful"
+	return true, "Create Computer Successful", computer
 }
 
 func FindComputer(IdComputer string) (bool, string, models.Computer) {
@@ -47,24 +43,17 @@ func UpdateComputer(IdComputer string, new_compuer schemas.ComputerUpdateSchema)
 	if err := config.DB.First(&computer, "id_computer = ?", IdComputer).Error; err != nil {
 		return false, "No Find Computer", computer
 	} else {
-		computer.Name = copyField(new_compuer.Name, computer.Name, "")
-		computer.Type = copyField(new_compuer.Type, computer.Type, "")
-		computer.State = copyField(new_compuer.State, computer.State, "")
-		computer.Message = copyField(new_compuer.Message, computer.Message, "")
-		computer.Pos_x = copyField(new_compuer.Pos_x, computer.Pos_x, 0.0)
-		computer.Pos_y = copyField(new_compuer.Pos_y, computer.Pos_y, 0.0)
+		computer.Name = tools.CopyField(new_compuer.Name, computer.Name, "")
+		computer.Type = tools.CopyField(new_compuer.Type, computer.Type, "")
+		computer.State = tools.CopyField(new_compuer.State, computer.State, "")
+		computer.Message = tools.CopyField(new_compuer.Message, computer.Message, "")
+		computer.Pos_x = tools.CopyField(new_compuer.Pos_x, computer.Pos_x, 0.0)
+		computer.Pos_y = tools.CopyField(new_compuer.Pos_y, computer.Pos_y, 0.0)
 
 		config.DB.Save(&computer)
 	}
 
 	return true, "Change Name Successful", computer
-}
-
-func copyField[T string | float32 | int](src T, des T, default_value T) T {
-	if src != default_value {
-		des = src
-	}
-	return des
 }
 
 /*
