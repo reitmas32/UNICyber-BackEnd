996935454dfd6cebb1b3d963812e0210f8d5cc4e
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-11 20:16:34 -0600

    feta: create new endpoint by create loan by account_number

--- src/api/v1/routes/loan.go
+++ src/api/v1/routes/loan.go
@@ -11,6 +11,9 @@ func LoanRoutes() {
 	//Create Computer
 	config.Router.POST(fmt.Sprintf("/api/%s/loan-computer", config.API_VERSION), views.Loan_POST)
 
+	//Create Computer by AccountNumber
+	config.Router.POST(fmt.Sprintf("/api/%s/loan-computer-by-account-number", config.API_VERSION), views.LoanByAccountNumber_POST)
+
 	//Get Computer
 	//config.Router.GET(fmt.Sprintf("/api/%s/loan-computer/:id", config.API_VERSION), views.Student_GET)
 
