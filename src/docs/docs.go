e846ca1358ae990d4730a180c7dc0a5089eabca2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-17 22:05:18 -0600

    docs: Update Docs Swagger

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -1552,7 +1552,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "https://unicyber-api-docs.onrender.com/",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "UNICyber-API",
