494725850329458544e66ed37bdafdcb3efa55fc
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-28 23:53:08 -0600

    docs: Add Docs by LinkAccountCode Schemas

--- src/api/v1/services/link_account_code.go
+++ src/api/v1/services/link_account_code.go
@@ -5,6 +5,31 @@ import (
 	"github.com/UNIHacks/UNIAccounts-BackEnd/src/models"
 )
 
+// CreateLinkAccountCode creates a link account code by persisting the provided LinkAccountCode object in the database.
+// It returns a tuple containing a boolean value indicating the success or failure of the operation,
+// a string message describing the result, and the created LinkAccountCode object.
+//
+// Parameters:
+//
+//	linkAccountCode (LinkAccountCode): The LinkAccountCode object representing the link account code to be created.
+//
+// Returns:
+//
+//	success (bool): A boolean value indicating the success (true) or failure (false) of the create operation.
+//	message (string): A string message providing information about the result of the create operation.
+//	linkAccountCode (LinkAccountCode): The created LinkAccountCode object, including any modifications made during the create operation
+//	                                   (such as auto-generated IDs or timestamps).
+//
+// Example:
+//
+//	linkCode := LinkAccountCode{UserName: "john_doe", Code: "569778", IdComputerLab: 1}
+//	success, message, createdLinkCode := CreateLinkAccountCode(linkCode)
+//	if success {
+//	    fmt.Println(message)
+//	    fmt.Println("Created LinkAccountCode:", createdLinkCode)
+//	} else {
+//	    fmt.Println("Error:", message)
+//	}
 func CreateLinkAccountCode(linkAccountCode models.LinkAccountCode) (bool, string, models.LinkAccountCode) {
 
 	result := config.DB.Create(&linkAccountCode)
@@ -15,6 +40,31 @@ func CreateLinkAccountCode(linkAccountCode models.LinkAccountCode) (bool, string
 	return true, "Create Code Successful", linkAccountCode
 }
 
+// FindLinkAccountCode retrieves a link account code from the database based on the provided code and user_name.
+// It returns a tuple containing a boolean value indicating the success or failure of the operation,
+// a string message describing the result, and the retrieved LinkAccountCode object.
+//
+// Parameters:
+//
+//	code (string): The code of the link account code to retrieve from the database.
+//	user_name (string): The user name associated with the link account code.
+//
+// Returns:
+//
+//	success (bool): A boolean value indicating the success (true) or failure (false) of the retrieval operation.
+//	message (string): A string message providing information about the result of the retrieval operation.
+//	linkAccountCode (models.LinkAccountCode): The retrieved LinkAccountCode object, if found in the database.
+//	                                          Otherwise, it will contain default values for its fields.
+//
+// Example:
+//
+//	success, message, linkCode := FindLinkAccountCode("569778", "john_doe")
+//	if success {
+//	    fmt.Println(message)
+//	    fmt.Println("Found LinkAccountCode:", linkCode)
+//	} else {
+//	    fmt.Println("Error:", message)
+//	}
 func FindLinkAccountCode(code string, user_name string) (bool, string, models.LinkAccountCode) {
 
 	var linkAccountCode models.LinkAccountCode
